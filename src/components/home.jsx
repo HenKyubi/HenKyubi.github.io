e6c0260a879087628df198235eb04e6abe4c1441
HenKyubi666 <jhonnino@unisangil.edu.co>
2022-01-29 18:26:39 -0500

    before to break

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,6 +14,30 @@ const defaultOptions = {
   },
 }
 
-const Home = () => <div></div>
+const Home = () => (
+  <div id="home" className="masthead masked">
+    <div className="">
+      <div className="container">
+        <h1>
+          Hi, i'm <br />
+          John Niño
+        </h1>
+        <p className="lead-text wow fadeInUp">
+          Front-End Developer &amp; Freelancer
+        </p>
+        <div className="control">
+          <a
+            href="#request"
+            className="btn wow fadeInUp"
+            data-wow-delay="0.2s"
+            data-toggle="modal"
+          >
+            Contact me
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+)
 
 export default Home
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,67 +16,15 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 
 import Header from "../components/header"
+import Home from "../components/home"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  // <div>
-  //   <Swiper
-  //     direction={"vertical"}
-  //     slidesPerView={1}
-  //     spaceBetween={30}
-  //     mousewheel={true}
-  //     pagination={{
-  //       clickable: true,
-  //     }}
-  //     modules={[Mousewheel, Pagination]}
-  //     className="mySwiper"
-  //   >
-  //     {/* <SwiperSlide>Slide 1</SwiperSlide>
-  //   <SwiperSlide>Slide 2</SwiperSlide>
-  //   <SwiperSlide>Slide 3</SwiperSlide>
-  //   <SwiperSlide>Slide 4</SwiperSlide>
-  //   <SwiperSlide>Slide 5</SwiperSlide>
-  //   <SwiperSlide>Slide 6</SwiperSlide>
-  //   <SwiperSlide>Slide 7</SwiperSlide>
-  //   <SwiperSlide>Slide 8</SwiperSlide>
-  //   <SwiperSlide>Slide 9</SwiperSlide> */}
-  //   </Swiper>
-  // </div>
-
-  // <Layout>
-  // <Seo title="Home" />
   <>
     <Header />
-    <main
-      id="home"
-      className="masthead masked"
-      data-stellar-background-ratio="0.8"
-    >
-      <div className="opener rel-1">
-        <div className="container">
-          <h1>
-            Hi, i'm <br />
-            John Niño
-          </h1>
-          <p className="lead-text wow fadeInUp">
-            Front-End Developer &amp; Freelancer
-          </p>
-          <div className="control">
-            <a
-              href="#request"
-              className="btn wow fadeInUp"
-              data-wow-delay="0.2s"
-              data-toggle="modal"
-            >
-              Contact me
-            </a>
-          </div>
-        </div>
-      </div>
-    </main>
+    <Home />
   </>
-  // </Layout>
 )
 
 export default IndexPage
