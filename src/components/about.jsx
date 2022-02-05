5b0d77f62e2353d58929078eafc7607567f4825b
HenKyubi666 <jhonnino@unisangil.edu.co>
2022-02-05 11:10:36 -0500

    about ok

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -26,27 +26,27 @@ const defaultOptionsMoon = {
 }
 
 const About = () => (
-  <section id="about" className="container d-flex">
+  <section id="about" className="container d-flex align-items-center">
     <div className="row align-items-center">
-      <div className="col-12 col-md-6 d-flex justify-content-center">
+      <div className="col-12 col-md-6 d-flex justify-content-center pb-3 pb-md-0">
         <img className="img-fluid img-me" alt="Picture of John Niño" src={Me} />
       </div>
-      <div className="col-12 col-md-6 text-about">
-        <div className="d-flex justify-content-center container-animation-moon">
-          <div className="animation-moon">
+      <div className="col-12 col-md-6 d-flex flex-column">
+        <div className="d-flex">
+          <div className="d-flex justify-content-center animation-moon">
             <div>
               <Lottie options={defaultOptionsMoon} height={250} width={250} />
             </div>
           </div>
-        </div>
-        <div>
-          <h2 className="text-center text-md-start fs-1 fw-bold">About me</h2>
-          <p className="pagraph-about fs-6 lh-sm">
-            Systems enginner - junior full-stack developer, skills in React,
-            Angular, HTML, CSS, JavaScript, NodeJS, Responsive Design. Knowledge
-            of database engines (MySQL, MongoDB), handling of GIT version
-            control.
-          </p>
+          <div className="text-about">
+            <h2 className="text-center text-md-start fs-1 fw-bold">About me</h2>
+            <p className="pagraph-about fs-6 lh-sm">
+              Systems enginner - junior full-stack developer, skills in React,
+              Angular, HTML, CSS, JavaScript, NodeJS, Responsive Design.
+              Knowledge of database engines (MySQL, MongoDB), handling of GIT
+              version control.
+            </p>
+          </div>
         </div>
         <div className="skills-bar justify-content-center row">
           <div>
