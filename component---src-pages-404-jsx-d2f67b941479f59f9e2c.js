45dc5a07e62bcac99c084f21dd1a34a78a050e67
HenKyubi666 <jhonnino@unisangil.edu.co>
2022-08-17 21:26:38 -0500

    Updates

---
+++ component---src-pages-404-jsx-d2f67b941479f59f9e2c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[256],{1657:function(t,e,n){n.r(e);var u=n(7294),a=n(809),l=n(3751);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-d2f67b941479f59f9e2c.js.map
